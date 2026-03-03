0ad97dfa96b93de6cf37b26c9d150c52bf651cb6
Ahmet Erdogan <ahmeterdogan@Ahmets-MacBook-Pro.local>
2026-03-03 13:51:28 +0000

    changedbucketnamelalalala

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "ce-bootcamp-tfstate-ahmeterdogan"
+    bucket         = "ce-bootcamp-tfstate-ahhdowurgehillmeterdoganov10"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
